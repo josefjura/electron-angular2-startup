8c4551e6909adc1e7263b1605a926f69e93d1127
jurajosef <josef.jura@hotmail.com>
2015-12-23 10:29:08 +0100

    Moved to HashLocationStrategy, removed lite-server

--- src/app/app.boot.ts
+++ src/app/app.boot.ts
@@ -3,8 +3,10 @@
 // Angular 2
 import {bootstrap} from 'angular2/platform/browser';
 
+import {provide} from 'angular2/core';
+
 // Angular's router injectables services/bindings
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 import {SERVICES_PROVIDERS} from './services/services';
 
@@ -14,6 +16,7 @@ bootstrap(
     App,
     [
         SERVICES_PROVIDERS,
-        ROUTER_PROVIDERS
+        ROUTER_PROVIDERS,
+        provide(LocationStrategy, { useClass: HashLocationStrategy })
     ]
 );
