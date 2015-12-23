3cf243ea3cb1edfbd8d2d1fd7f7a4fc2a585af02
jurajosef <josef.jura@hotmail.com>
2015-12-23 13:43:16 +0100

    Added compile-with-maps task, will be used in release tasks later

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,42 @@ var gutil = require("gulp-util");
 var tsc = require("gulp-typescript");
 var less = require('gulp-less');
 var jetpack = require('fs-jetpack');
+var sourcemaps = require('gulp-sourcemaps');
 
 var src = jetpack.cwd('src');
 var dest = jetpack.cwd('build');
 
 gulp.task('clean', function () {
-	return dest.dir('.', { empty: true });
+    return dest.dir('.', { empty: true });
 })
 
 gulp.task("copy", ['clean'], function () {
-	src.copy('.', dest.path(), {
-		overwrite: true,
-		matching: ['*.html', '*.png', '*.jpg', '*.json', '!./node_modules/**/*']
-	});
+    src.copy('.', dest.path(), {
+        overwrite: true,
+        matching: ['*.html', '*.png', '*.jpg', '*.json', '!./node_modules/**']
+    });
 });
 
 gulp.task("copy-node", ['clean'], function () {
-	src.copy('.', dest.path(), {
-		overwrite: true,
-		matching: ['./node_modules/**/*']
-	});
+    src.copy('.', dest.path(), {
+        overwrite: true,
+        matching: ['./node_modules/**']
+    });
 });
 
 var tsProject = tsc.createProject('tsconfig.json');
 gulp.task("compile", ['clean'], function (callback) {
-	var tsResult = tsProject.src()
-		.pipe(tsc(tsProject));
+    return tsProject.src()
+        .pipe(tsc(tsProject))
+        .pipe(gulp.dest('build'))
+});
 
-	return tsResult.js.pipe(gulp.dest('build'));
+gulp.task("compile-with-maps", ['clean'], function (callback) {
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tsProject))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('build'))
 });
 
 gulp.task("build", ['compile', 'copy', 'copy-node', 'less'], function () {
@@ -38,7 +46,7 @@ gulp.task("build", ['compile', 'copy', 'copy-node', 'less'], function () {
 });
 
 gulp.task('less', ['clean'], function () {
-	return gulp.src(src.path('stylesheets/main.less'))
+    return gulp.src(src.path('stylesheets/main.less'))
         .pipe(less())
         .pipe(gulp.dest(dest.path('stylesheets')));
 });
