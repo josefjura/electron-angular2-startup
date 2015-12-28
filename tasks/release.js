8ce891c8dc9bdae03477aebbf7b060ce915dcdf7
jurajosef <josef.jura@hotmail.com>
2015-12-28 14:17:02 +0100

    Changed release scripts to fix some problems

--- tasks/release.js
+++ tasks/release.js
@@ -1,44 +1,64 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var packager = require('electron-packager');
-var jetpack = require('fs-jetpack');
-
-
 var createInstaller = require('electron-installer-squirrel-windows');
-var lastPackageUrl = "";
+var jetpack = require('fs-jetpack');
+var env = jetpack.read('./src/package.json', 'json');
+var argv = require('yargs').argv;
 
 var temp = jetpack.cwd('temp');
+var build = jetpack.cwd('build');
+var release = jetpack.cwd('release');
 
 gulp.task('clear', function () {
     return temp.dir('.', { empty: true });
 });
 
 
-gulp.task('package', ['clear'], function (cb) {
+gulp.task('release', ['clear'], function (cb) {
+    var platform = argv.platform || 'win32';
+
     packager({
-        dir: './build',
-        out: './temp',
-        name: 'appName',
-        platform: 'win32',
-        arch: 'x64',
-        version: '0.36.0'
+        dir: build.path(),
+        out: temp.path(),
+        asar: true,
+        name: env.name,
+        platform: platform,
+        arch: 'all',
+        version: '0.36.1'
     }, function done(err, appPath) {
-        lastPackageUrl = appPath.toString();
+        if (err || !appPath) {
+            gutil.log(err);
+            cb(err);
+            return;
+        };
+
+        create(appPath);
         cb(err);
     });
 });
 
-gulp.task('release', ['package'], function (cb) {
-    createInstaller(
-        {
-            out: './release',
-            authors : "Josef Jura",
-            name: 'appName',
-            path: lastPackageUrl
-        },
-        function (err) {
-            cb(err);
-        }
-        );
-})
 
+function create(appPath) {
+    for (var pathIndex in appPath) {
+        if (appPath.hasOwnProperty(pathIndex)) {
+            var path = appPath[pathIndex];
+
+            createInstaller(
+                {
+                    out: release.path(env.version),
+                    authors: env.author,
+                    name: env.name,
+                    product_name: env.name,
+                    path: path,
+                    version: env.version,
+                    debug: true,
+                    exe: 'AppSetup.exe'
+                },
+                function (err) {
+                    gutil.log(err);
+                }
+                );
+        }
+    }
+}
